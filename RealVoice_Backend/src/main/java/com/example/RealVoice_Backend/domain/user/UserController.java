e2e18b470951c678119fa5287bfb8e1ccdaac359
kangwhon <kangwhon@naver.com>
2024-04-22 18:06:12 +0900

    수정 및 추가

--- RealVoice_Backend/src/main/java/com/example/RealVoice_Backend/domain/user/UserController.java
+++ RealVoice_Backend/src/main/java/com/example/RealVoice_Backend/domain/user/UserController.java
@@ -1,61 +1,57 @@
 package com.example.RealVoice_Backend.domain.user;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // 서버 담당
-
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
     private UserRepository userRepository;
 
-    // "user"라는 이름으로 새로운 'User' 객체를 생성하여 모델에 추가
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("user", new User());
-        return "register";
+	@GetMapping("/register")
+    public String showRegistrationForm() {
+        return "Registration Form";
     }
 
-    // 'user' 객체를 MongoDB 데이터베이스에 저장
     @PostMapping("/register")
-    public String registerUser(User user) {
+    public String registerUser(@RequestBody User user) {
+        user.setCreated_at(new Date());
         userRepository.save(user);
-        return "redirect:/login";
+        return "User Registered Successfully";
     }
 
-    // 로그인 페이지
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login";
+        return "Login Form";
     }
 
-    // 로그인시 등록되어있는 사용자인지 확인
     @PostMapping("/login")
-    public String loginUser(User user) {
+    public String loginUser(@RequestBody User user) {
         User existingUser = userRepository.findByUsername(user.getUsername());
         if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
-            return "redirect:/home";
+            return "Login Successful";
         } else {
-            return "login";
+            return "Login Failed";
         }
     }
-    
-    // 홈페이지
+
     @GetMapping("/home")
     public String showHomePage() {
-        return "home";
+        return "Home Page";
     }
 
-    // 회원정보 목록 출력
-    @GetMapping("/users")
-    public String listUsers(Model model) {
-        List<User> userList = userRepository.findAll();
-        model.addAttribute("userList", userList);
-        return "users";
+    @GetMapping("/all")
+    public List<User> listUsers() {
+        return userRepository.findAll();
     }
 }
